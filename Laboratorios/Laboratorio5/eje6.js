32e84ed1e8c7c08a7b9e6403ad4538da027d6bb2
BricardoL <lbryanrick@gmail.com>
2019-09-27 10:43:27 -0600

    Eje6 y eje8

--- Laboratorios/Laboratorio5/eje6.js
+++ Laboratorios/Laboratorio5/eje6.js
@@ -2,7 +2,12 @@ function ordenarascendente(array = [1. 5. 8. 2. 4. 6]){
     let a, b;
     for(j=0; j<array.length -1; j++){
         for(i=0; i<array.length -1; i++){
-            if(array{})
+            if(array[i] > array[i + 1]){
+                a = array[i];
+                b= array[i + 1];
+                array[i]=b;
+                array[i + 1] = a;
+            }
         }
-    }
+    } return console.log(array);
 }
